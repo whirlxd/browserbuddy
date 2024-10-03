896f9dd2324317b7c33febfb55456f7481f22e25
JosiasAurel <ndjosiasaurel@gmail.com>
2024-10-03 09:20:31 +0100

    create endpoint to receive email submissions

---
+++ src/pages/api/signup.ts
@@ -0,0 +1,35 @@
+import Airtable from "airtable";
+import type { APIRoute } from "astro";
+
+console.log(import.meta.env.BROWSER_BUDDY_AIRTABLE_PAT);
+
+Airtable.configure({
+    endpointUrl: "https://api.airtable.com",
+    apiKey: import.meta.env.BROWSER_BUDDY_AIRTABLE_PAT
+});
+
+const base = Airtable.base(import.meta.env.BROWSER_BUDDY_AIRTABLE_ID!);
+
+export const POST: APIRoute = async ({ request }) => {
+    const body = await request.json();
+    const { email } = body;
+
+    try {
+        const result = await new Promise((resolve, reject) => {
+            base("Email SignUp").create([
+                {
+                    fields: {
+                        Email: email
+                    }
+                }
+            ], (err: any, records: any) => {
+                if (err) reject(err);
+                resolve(records)
+            })
+        });
+
+        return new Response(JSON.stringify({ success: true, result }));
+    } catch (error) {
+        return new Response(JSON.stringify({ success: false, error }));
+    }
+};
