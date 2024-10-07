60b9e10e8a4fc0dbb292f3e9856f0e5ec8d5118c
JosiasAurel <ndjosiasaurel@gmail.com>
2024-10-07 17:48:16 +0100

    add vercel adapter for astrojs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
+  output: "hybrid",
+  adapter: vercel(),
 });
