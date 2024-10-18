ccaf23c28b3483d1afe2a2f300fe9ce4c026f5d1
JosiasAurel <ndjosiasaurel@gmail.com>
2024-10-18 16:59:36 +0100

    format document and abtract qa questions in component

--- website/src/pages/api/signup.ts
+++ website/src/pages/api/signup.ts
@@ -4,32 +4,35 @@ import type { APIRoute } from "astro";
 console.log(import.meta.env.BROWSER_BUDDY_AIRTABLE_PAT);
 
 Airtable.configure({
-    endpointUrl: "https://api.airtable.com",
-    apiKey: import.meta.env.BROWSER_BUDDY_AIRTABLE_PAT
+  endpointUrl: "https://api.airtable.com",
+  apiKey: import.meta.env.BROWSER_BUDDY_AIRTABLE_PAT,
 });
 
 const base = Airtable.base(import.meta.env.BROWSER_BUDDY_AIRTABLE_ID!);
 
 export const POST: APIRoute = async ({ request }) => {
-    const body = await request.json();
-    const { email } = body;
+  const body = await request.json();
+  const { email } = body;
 
-    try {
-        const result = await new Promise((resolve, reject) => {
-            base("Email SignUp").create([
-                {
-                    fields: {
-                        Email: email
-                    }
-                }
-            ], (err: any, records: any) => {
-                if (err) reject(err);
-                resolve(records)
-            })
-        });
+  try {
+    const result = await new Promise((resolve, reject) => {
+      base("Email SignUp").create(
+        [
+          {
+            fields: {
+              Email: email,
+            },
+          },
+        ],
+        (err: any, records: any) => {
+          if (err) reject(err);
+          resolve(records);
+        },
+      );
+    });
 
-        return new Response(JSON.stringify({ success: true, result }));
-    } catch (error) {
-        return new Response(JSON.stringify({ success: false, error }));
-    }
+    return new Response(JSON.stringify({ success: true, result }));
+  } catch (error) {
+    return new Response(JSON.stringify({ success: false, error }));
+  }
 };
