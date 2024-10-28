0c8de07d414ea1f04cf329e19776c1692ed3014b
SkyfallWasTaken <mahadkalam1@proton.me>
2024-10-28 15:42:52 +0000

    Listen on all events

--- submissions/slack-status/src/background.js
+++ submissions/slack-status/src/background.js
@@ -10,11 +10,14 @@ async function getSlackConfig() {
         active: false
     });
     if (!slackTab.id) throw new Error("Failed to create tab");
+    console.debug(`Created Slack tab: ${slackTab.id}`)
+
     const results = await chrome.scripting.executeScript({
         target: { tabId: slackTab.id },
         func: () => localStorage.getItem("localConfig_v2"),
         injectImmediately: true
     });
+
     const localConfig = results[0].result;
     if (!localConfig) throw new Error("Failed to get xoxc");
     const config = JSON.parse(localConfig);
@@ -34,26 +37,32 @@ async function getSlackConfig() {
 
 async function main() {
     const config = await getSlackConfig()
-    chrome.tabs.onActivated.addListener(async (activeInfo) => {
-        console.log("Tab activated")
-        const tab = await chrome.tabs.get(activeInfo.tabId);
-        if (!tab.title) return;
+    chrome.tabs.onActivated.addListener((activeInfo) => updateStatus(activeInfo.tabId, config));
+    chrome.tabs.onUpdated.addListener((tabId) => updateStatus(tabId, config));
+}
 
-        const data = new FormData();
-        data.append('token', config.xoxc);
-        data.append('profile', JSON.stringify({
-            status_emoji: ':globe_with_meridians:',
-            status_text: `On ${tab.title}`
-        }))
-        await fetch(`https://${config.teamDomain}.slack.com/api/users.profile.set`, {
-            method: 'POST',
-            body: data,
-            headers: {
-                Cookie: `d=${encodeURIComponent(config.xoxd)}`
-            }
-        })
-        console.log("Updated status to", tab.title)
-    });
+/**
+ * @type (tabId: number, config: { xoxc: string, xoxd: string, teamDomain: string }) => Promise<void>
+ */
+async function updateStatus(tabId, config) {
+    console.debug("Tab activated:", tabId)
+    const tab = await chrome.tabs.get(tabId);
+    if (!tab.title) return;
+
+    const data = new FormData();
+    data.append('token', config.xoxc);
+    data.append('profile', JSON.stringify({
+        status_emoji: ':globe_with_meridians:',
+        status_text: `On ${tab.title}`
+    }))
+    await fetch(`https://${config.teamDomain}.slack.com/api/users.profile.set`, {
+        method: 'POST',
+        body: data,
+        headers: {
+            Cookie: `d=${encodeURIComponent(config.xoxd)}`
+        }
+    })
+    console.log("Updated status to", tab.title)
 }
 
 main();
