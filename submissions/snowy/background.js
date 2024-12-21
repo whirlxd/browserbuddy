dd29c6be18946bdeb90032cb79f39b9d8668601c
pizzalover125 <119770527+pizzalover125@users.noreply.github.com>
2024-12-21 09:26:03 -0700

    Update background.js

--- submissions/snowy/background.js
+++ submissions/snowy/background.js
@@ -1,6 +1,8 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content.js']
-  });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ['content.js']
+    });
+  }
 });
